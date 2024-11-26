f147a9127b6e6a5b5bc0bb86c2ed4a11498dfc47
akhad <abdoul-sattar.djobo@student.fh-kiel.de>
2024-11-26 15:00:26 +0100

    Update waffe.py

--- waffe.py
+++ waffe.py
@@ -1,1 +1,19 @@
-pip install pygame
+import pygame
+
+#load bullet image
+
+bullet_img = pygame.image.load(" ").convert_alpha()
+
+
+class Bullet(pygame.sprite.Sprite):
+
+  def __init__(self, x , y, direction):
+    
+    pygame.sprite.Sprite__init__(self)
+    self.speed = 10
+    self.image = bullet_img
+    self.rect = self.image.get_rect()
+    self.rect.center = (x , y)
+    self.direction = direction 
+
+  
