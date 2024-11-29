6b5c088dbfc0924cb0792df62161dbe2a8509b25
Mehtikaan <mehtikaan.orman@student.fh-kiel.de>
2024-11-29 04:30:21 +0100

    doit

---
+++ button.py
@@ -0,0 +1,16 @@
+import pygame
+import animationen as am
+import configparser as cp
+import config_einstellungen as bib
+# Werte aus der Konfiguration laden und konvertieren
+try:
+    HEIGHT = int(config.get("Fenster", "height"))
+    WIDTH = int(config.get("Fenster", "width"))
+
+except Exception as e:
+    print("Fehler beim Laden der Konfigurationswerte:", e)
+    pygame.quit()
+    exit()
+class Button:
+    def __init__(self,image):
+        pass
