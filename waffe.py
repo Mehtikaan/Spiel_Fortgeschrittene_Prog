cf0abded7666ecddccae7b67c6d9e3075f2dbb17
akhad <abdoul-sattar.djobo@student.fh-kiel.de>
2024-12-01 21:39:30 +0100

    Update waffe.py
    kunai als bullet

--- waffe.py
+++ waffe.py
@@ -1,11 +1,16 @@
 import pygame
+import os
 
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.Surface((10, 5))  # A simple rectangle bullet
-        self.image.fill((255, 0, 0))  # Red color for the bullet
+
+        game_folder = os.path.dirname(__file__)
+         # Kunai-Bild laden und skalieren
+        self.image = pygame.image.load(os.path.join(game_folder, '_image', "Kunai.png")).convert()
+        self.image = pygame.transform.scale(self.image, (20, 10))  # Größe anpassen
+
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         self.speed = 10  # Bullet speed
@@ -16,4 +21,5 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.x += self.speed
         if self.rect.x > pygame.display.get_surface().get_width():
             self.kill()
+  
   
