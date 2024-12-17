40dc1ae56877aa9b989a1ad5d3978e4543251b33
Hasso <husseinmak61@gmail.com>
2024-12-17 23:14:22 +0100

    fehler

--- Main-2.py
+++ Main-2.py
@@ -44,7 +44,6 @@ font = pygame.font.Font(None, 56)  # Standard-Schriftart, Größe 56
 start_music_channel = snd.start_music.play()
 start_music_channel = pygame.mixer.Channel(0)  # Reserviere Kanal 0
 start_music_channel.play(snd.start_music)
-level_music = gl.level_music
 
 
 background = img.background
@@ -110,6 +109,119 @@ if score <1000:
 else:
     pass
 last_spawn_time = pygame.time.get_ticks()
+
+#Level Wechsler
+current_level = 0
+
+level_music = {
+   # 0: "zombie_music.wav",
+    1: os.path.join(game_folder, '_sounds',"cowboy_music.wav"),
+    2: os.path.join(game_folder, '_sounds',"robot_music.wav"),
+    3: os.path.join(game_folder, '_sounds',"knight_music.wav"),
+    4: os.path.join(game_folder, '_sounds',"wind-blowing-sfx-12809.mp3"),
+    5: os.path.join(game_folder, '_sounds',"pumpkin_music.wav"),
+    6: os.path.join(game_folder, '_sounds',"courli_music.wav")
+}
+
+pygame.mixer.music.set_volume(0.3)  # Lautstärke auf 50% einstellen
+
+def level_changer():
+   global platform_image, background, current_level, level_music, start_music_channel, trap_image
+   level_texts = {
+       1: "Wieso sehe ich nichts mehr ?!! Was passiert hier...?",
+       2: "Puh war das heiß, ich verstehe nicht wo ich bin.",
+       3: "Nicht schon wieder!!",
+       4: "Frohe Weihnachten! Jetzt kommen die Santa-Gegner.",
+       5: "Das Kürbislevel! Nichts für schwache Nerven.",
+       6: "Warte mal..."    
+    }
+
+
+   if score >= 1000.0 and current_level == 0:
+       if start_music_channel:  # Prüfen, ob Kanal existiert
+        print("Stopping music on channel:", start_music_channel)
+        start_music_channel.stop()
+        start_music_channel = None
+       else:
+           print("start_music_channel does not exist or is None!")
+       print("eeee1")
+   if score >= 1000 and current_level < 1:
+       current_level = 1
+       sqn.transition_sequence() 
+       platform_image = img.platform_image_level_1
+       background = img.background_level_1
+       pygame.mixer.music.load(level_music[1])  # Lade Level-1-Musik
+       pygame.mixer.music.play(-1)  # Endlosschleife
+       sqn.fade(screen1, BLACK, 1, fade_out=True, text=level_texts[1], font=font)
+       for enemy in all_zombies:
+           enemy.kill()
+
+       print("eeee1")
+
+   elif score >= 2000 and current_level < 2 :
+      current_level = 2
+      sqn.transition_sequence() 
+      platform_image = img.platform_image_level_2
+      background = img.background_level_2
+      pygame.mixer.music.load(level_music[2])  # Lade Level-2-Musik
+      pygame.mixer.music.play(-1)
+      sqn.fade(screen1, BLACK, 1, fade_out=True, text=level_texts[2], font=font)
+      for enemy in all_zombies:
+           enemy.kill()
+
+      print("eeee1")
+
+   elif score >= 3000 and current_level < 3:
+       current_level = 3
+       sqn.transition_sequence() 
+       platform_image = img.platform_image_level_3
+       background = img.background_level_3
+       pygame.mixer.music.load(level_music[3])  # Lade Level-3-Musik
+       pygame.mixer.music.play(-1)
+       sqn.fade(screen1, BLACK, 1, fade_out=True, text=level_texts[3], font=font)
+       for enemy in all_zombies:
+           enemy.kill()
+
+       print("eeee1")
+    
+           
+   elif score >= 4000 and current_level < 4:
+       current_level = 4
+       sqn.transition_sequence() 
+       platform_image = img.platform_image_level_4
+       background = img.background_level_4
+       pygame.mixer.music.load(level_music[4])  # Lade Level-4-Musik
+       pygame.mixer.music.play(-1)
+       sqn.fade(screen1, BLACK, 1, fade_out=True, text=level_texts[4], font=font)  # Zeichne alle Sprites
+       for enemy in all_zombies:
+           enemy.kill()
+             
+       print("eeee1")
+
+   elif score >= 5000 and current_level < 5:
+       current_level = 5
+       sqn.transition_sequence() 
+       platform_image = img.platform_image_level_5
+       background = img.background_level_5
+       pygame.mixer.music.load(level_music[5])  # Lade Level-5-Musik
+       pygame.mixer.music.play(-1)
+       sqn.fade(screen1, BLACK, 1, fade_out=True, text=level_texts[5], font=font)
+       for enemy in all_zombies:
+           enemy.kill()
+       print("eeee1")
+
+   elif score >= 6000 and current_level < 6:
+       current_level = 6
+       sqn.transition_sequence() 
+       platform_image = img.platform_image_level_6
+       background = img.background_level_6
+       pygame.mixer.music.load(level_music[6])  # Lade Level-6-Musik
+       pygame.mixer.music.play(-1)
+       sqn.fade(screen1, BLACK, 1, fade_out=True, text=level_texts[6], font=font)
+       for enemy in all_zombies:
+           enemy.kill()
+       print("eeee1")
+
 
 
 
@@ -284,7 +396,7 @@ while running:
                 if zombie.hp==0:
                     zombie.kill()
 
-    gl.level_changer(score=score, all_zombies=all_zombies, surface=screen1)
+    level_changer()
     # Prüfen, ob Lebenspunkte <= 0 sind
     if main_charakter.health_points <= 0:
         snd.death_sound.play()
