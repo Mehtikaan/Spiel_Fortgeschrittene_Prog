db00dc2c448b2670e58f2ceac4edf97e54442153
Hasso <husseinmak61@gmail.com>
2024-12-18 21:52:43 +0100

    Changes

--- traps.py
+++ traps.py
@@ -2,23 +2,19 @@ import pygame
 
 class Trap(pygame.sprite.Sprite):
     def __init__(self, x, y, surface, sprite_image, scale=(80, 50), speed=8):
-        """
-        Ein Hindernis, das von rechts nach links schwebt.
-        """
+
         super().__init__()
         self.surface = surface
-        self.image = pygame.transform.scale(sprite_image, scale)  # Hindernisbild skalieren
+        self.image = pygame.transform.scale(sprite_image, scale)
         self.rect = self.image.get_rect()
-        self.rect.midtop = (x, y)  # Startposition festlegen
-        self.speed = speed  # Bewegungsgeschwindigkeit
-        self.start_x = x  # Speichert die Startposition
+        self.rect.midtop = (x, y)  
+        self.speed = speed 
+        self.start_x = x 
 
     def update(self):
-        """Bewegung von rechts nach links."""
         self.rect.x -= self.speed
-        if self.rect.right < 0:  # Wenn das Hindernis aus dem Bildschirm geht
-            self.rect.left = 4000  # Zurück an den rechten Rand setzen
+        if self.rect.right < 0:  
+            self.rect.left = 4000 
 
     def draw(self):
-        """Hindernis auf dem Bildschirm rendern."""
         self.surface.blit(self.image, self.rect)
